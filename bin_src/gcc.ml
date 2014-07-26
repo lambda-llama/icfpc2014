f0779ffefd1b659033779d3e97ef02b3c32fef6a
Sergei Lebedev <superbobry@gmail.com>
2014-07-26 16:15:48 +0400

    Print error in 'gcc.ml'

--- bin_src/gcc.ml
+++ bin_src/gcc.ml
@@ -1,9 +1,12 @@
 open Sexplib
+open Sexplib.Std
 
 open Gcc_types
 
 let () =
-  let sexp = Sexp.input_sexp stdin in
-  let expr = expr_of_sexp sexp in
-  let open Gcc_compiler in
-  compile expr |> assemble |> print_endline
+  try
+    let sexp = Sexp.input_sexp stdin in
+    let expr = expr_of_sexp sexp in
+    let open Gcc_compiler in
+    compile expr |> assemble |> print_endline
+  with e -> sexp_of_exn e |> Sexp.to_string |> print_endline
