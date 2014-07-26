9d8089cab7c96dfef580c9d9ddea0b52c4a78261
Sergei Lebedev <superbobry@gmail.com>
2014-07-26 21:04:17 +0400

    Fixed indexing in world- actor- getters

--- strats/simple.clj
+++ strats/simple.clj
@@ -26,10 +26,11 @@
 (def GHOST 6)
 
 (defn world-map [world] (head world))
-(defn lambda-man [world] (nth world 2))
-(defn direction [actor] (nth actor 3))
-(defn location [actor] (nth actor 2))
-(defn at [world-map x y] (nth (nth (world-map) y) x))
+(defn lambda-man [world] (nth world 1))
+(defn location [actor] (nth actor 1))
+(defn direction [actor] (nth actor 2))
+(defn at [world-map xy]
+  (nth (nth (world-map) (head xy)) (tail xy)))
 
 (defn neighbour [pos direction]
   (let [x (head pos)
@@ -54,16 +55,19 @@
     (let [pos (lookup DIRECTIONS direction)]
       (nth DIRECTIONS (dec pos)))))
 
+(defn trace [x] (debug x x))
+
 (defn main [initial-world ghost-ai]
   (pair 0
         (fn [state world]
           (let [lm (lambda-man world)
+                wm (world-map world)
+                loc (location lm)
                 dir (direction lm)
-                loc (debug (location lm) (location lm))
                 t1 (next dir)
                 t2 (back t1)
                 t3 (back t2)
-                is-free (fn [t] (not (= WALL (neighbour loc t))))]
+                is-free (fn [t] (not (= WALL (at wm (neighbour loc t)))))]
             (pair state
                   (if (is-free t1)
                     t1
