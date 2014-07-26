1490ef948fd44800abca76a977c40f84fb1aaafd
Dmitry Groshev <lambdadmitry@gmail.com>
2014-07-26 14:30:11 +0400

    first take on tosexp

---
+++ tosexp/src/tosexp/core.clj
@@ -0,0 +1,19 @@
+(ns tosexp.core
+  (:use [clojure.core.match :only (match)])
+  (:require [clojure.walk :as w]))
+
+(defn transform [form]
+  (match
+   [(vec form)]
+   [['+ a b]] (list 'Add (transform a) (transform b))
+   [(a :guard number?)] (list 'Const a)
+   ))
+
+(defn mytest []
+  (let []
+    '(+ 1 2)))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
