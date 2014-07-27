21ab4379ee450f0f306227694374717b16f82059
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-07-27 12:57:50 +0400

    better build script for mac

--- compile.sh
+++ compile.sh
@@ -1,1 +1,7 @@
-cat $@ | java -jar ./tosexp.jar | ./gcc.native | xclip -i -selection clipboard
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    pbin="xclip -i -selection clipboard"
+else
+    pbin="pbcopy"
+fi
+
+cat $@ | java -jar ./tosexp.jar | ./gcc.native | $pbin
