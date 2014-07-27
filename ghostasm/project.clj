878e639eef6181a05a48ba1fdd1d1e7e6741634f
Dmitry Groshev <lambdadmitry@gmail.com>
2014-07-27 16:33:05 +0400

    THOSE FUCKERS FROM GITLAB BEAT ME UP PLZ HELP

--- ghostasm/project.clj
+++ ghostasm/project.clj
@@ -1,5 +1,6 @@
 (defproject ghostasm "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.match "0.2.1"]]
+                 [org.clojure/core.match "0.2.1"]
+                 [prismatic/plumbing "0.3.3"]]
   :main ghostasm.core
   :profiles {:uberjar {:aot :all}})
--- ghostasm/src/ghostasm/core.clj
+++ ghostasm/src/ghostasm/core.clj
@@ -3,69 +3,107 @@
   (:require [clojure.walk :as w])
   (:require [clojure.pprint :as pp])
   (:require [clojure.string :as s])
+  (:require [plumbing.core :as pl])
   (:gen-class))
 
 (defn MOV [dest src]
-  [:mov dest src])
+  {:type :op
+   :op "mov"
+   :args [dest src]})
 
 (defn INC [dest]
-  [:inc dest])
+  {:type :op
+   :op "inc"
+   :args [dest]})
 
 (defn DEC [dest]
-  [:dec dest])
+  {:type :op
+   :op "dec"
+   :args [dest]})
 
 (defn ADD [dest src]
-  [:add dest src])
+  {:type :op
+   :op "add"
+   :args [dest src]})
 
 (defn SUB [dest src]
-  [:sub dest src])
+  {:type :op
+   :op "sub"
+   :args [dest src]})
 
 (defn MUL [dest src]
-  [:mul dest src])
+  {:type :op
+   :op "mul"
+   :args [dest src]})
 
 (defn DIV [dest src]
-  [:div dest src])
+  {:type :op
+   :op "div"
+   :args [dest src]})
 
 (defn AND [dest src]
-  [:and dest src])
+  {:type :op
+   :op "and"
+   :args [dest src]})
 
 (defn OR [dest src]
-  [:or dest src])
+  {:type :op
+   :op "or"
+   :args [dest src]})
 
 (defn XOR [dest src]
-  [:xor dest src])
+  {:type :op
+   :op "xor"
+   :args [dest src]})
 
 (defn JLT [targ x y]
-  [:jlt targ x y])
+  {:type :op
+   :op "jlt"
+   :args [targ x y]})
 
 (defn JEQ [targ x y]
-  [:jeq targ x y])
+  {:type :op
+   :op "jeq"
+   :args [targ x y]})
 
 (defn JGT [targ x y]
-  [:jgt targ x y])
+  {:type :op
+   :op "jgt"
+   :args [targ x y]})
 
 (defn INT [i]
-  [:int i])
+  {:type :op
+   :op "int"
+   :args [i]})
 
 (defn HLT []
-  [:hlt])
+  {:type :op
+   :op "hlt"
+   :args []})
 
 (defn label [name]
-  [:label name])
+  {:type :label
+   :name name})
+
+(defn at [addr]
+  {:type :at
+   :addr addr})
 
 (defn asm [& insts]
   (vec insts))
 
+(defn format-arg [arg]
+  (cond
+   (keyword? arg) (name arg)
+   (string? arg) arg
+   (number? arg) (str arg)
+   (map? arg) (if (= :at (:type arg))
+                (str "[" (format-arg (:addr arg)) "]"))))
 
-
-(defn formatter [op & args]
-  (->> (for [arg args]
-         (cond
-          (keyword? arg) (name arg)
-          (string? arg) arg
-          (number? arg) (str arg)))
+(defn formatter [op]
+  (->> (map format-arg (:args op))
        (s/join ",")
-       (vector (name op))
+       (vector (:op op))
        (filter (complement s/blank?))
        (s/join " ")))
 
@@ -74,15 +112,12 @@
 (defn label-collect-phase [forms]
   (let [reducer
         (fn [acc form]
-          (match [form]
-                 [[:label label-name]]
-                 (update-in acc [:labels]
-                            assoc label-name (:counter acc))
-
-                 :else
-                 {:labels (:labels acc)
-                  :forms (conj (:forms acc) form)
-                  :counter (inc (:counter acc))}))]
+          (case (:type form)
+            :label (update-in acc [:labels]
+                              assoc (:name form) (:counter acc))
+            :op {:labels (:labels acc)
+                 :forms (conj (:forms acc) form)
+                 :counter (inc (:counter acc))}))]
     (reduce reducer
             {:labels {}
              :forms []
@@ -90,30 +125,101 @@
             forms)))
 
 (defn label-replace-phase [{:keys [labels forms]}]
-  (for [[op & args] forms]
-    (cons op
-          (for [arg args]
-            (if (and (vector? arg)
-                     (= (first arg) :label))
-              (labels (second arg)) ;; SAFE-GET HERE
-              arg)))))
+  (for [form forms]
+    (assoc form
+      :args (for [arg (:args form)]
+              (if (and (map? arg)
+                       (= (:type arg) :label))
+                (pl/safe-get labels (:name arg)) ;; SAFE-GET HERE
+                arg)))))
 
 (defn asm-compile [insts]
-  (->> insts
-       label-collect-phase
-       label-replace-phase
-       (map (partial apply formatter))
-       (s/join "\n")))
+  (let [code-by-str (->> insts
+                         flatten
+                         label-collect-phase
+                         label-replace-phase
+                         (map formatter))
+        code (s/join "\n" code-by-str)]
+    (println (str "len: " (count code-by-str) "/256\n" ))
+    code))
+
+(defn jmp [addr]
+  (JEQ addr 0 0))
+
+(declare int-by-name)
+
+(defn call-by-label [lbl]
+  [(MOV :h :pc) ;; return address in H register
+   (jmp (label lbl))])
+
+(defn return []
+  (MOV :pc :h))
+
+(defn int-by-name [name]
+  (INT (pl/safe-get {:direction 0
+                     :first-lambda 1
+                     :second-lambda 2
+                     :my-index 3
+                     :starting-pos 4
+                     :current-pos 5
+                     :ghost-info 6
+                     :map 7
+                     :dbg 8}
+                    name)))
 
 (defn mytest []
   (println
    (asm-compile
     (asm
-     (MOV :a 2)
-     (JLT (label :this) 2 3)
+     (MOV :a 0)
+     (MOV :b 0)
+     (MOV :c 255)
+
+     (label :min-loop-start)
+     (INC :c)
+     (JLT (label :min-loop-end) (at :c) :a)
+     (MOV :a (at :c))
+     (MOV :b :c)
+     (label :min-loop-end)
+     (int-by-name :dbg)
+     (JLT (label :min-loop-start) :c 3)
+
+     (MOV :a :b)
      (INT 0)
-     (label :this)
-     (HLT)))))
+     (INT 3)
+     (INT 6)
+     ;; (INC (at :b)) ;; change this
+     (call-by-label :find-lambdaman)
+     (HLT)
+
+     (label :find-lambdaman) ;; update first four adresses, uses a-d
+     (MOV (at 0) 0)
+     (MOV (at 1) 0)
+     (MOV (at 2) 0)
+     (MOV (at 3) 0)
+     (int-by-name :my-index) ;; index in a
+     (int-by-name :current-pos) ;; x in a, y in b
+     (MOV :c :a)
+     (MOV :d :b) ;; a and b will be needed, x now in c, y in d
+     (int-by-name :first-lambda) ;; x_lambda in a, y_lambda in b
+     (JLT (label :skip-increase-left) :a :c)  ;; if x_lambda < x, don't go left
+     (INC (at 3))
+     (label :skip-increase-left)
+     (JLT (label :skip-increase-down) :b :d) ;; if y_lambda < y, don't go down
+     (INC (at 2))
+     (label :skip-increase-down)
+     (JGT (label :skip-increase-right) :a :c) ;; if x_lambda > x, don't go right
+     (INC (at 1))
+     (label :skip-increase-right)
+     (JGT (label :skip-increase-up) :b :d) ;; if y_lambda > y, don't go right
+     (INC (at 0))
+     (label :skip-increase-up)
+     (MOV :d (at 0))
+     (MOV :e (at 1))
+     (MOV :f (at 2))
+     (MOV :g (at 3))
+     #_(int-by-name :dbg)
+     (return)))))
 
 
 (defn -main
